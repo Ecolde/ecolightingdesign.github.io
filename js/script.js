7c4adafefba9220f64d0e2fae24cdc09e6a3dce0
Ecolde <129002154+Ecolde@users.noreply.github.com>
2023-03-31 19:24:06 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,57 +3,30 @@ function w3_open() {
     document.getElementById("mySidebar").style.display = "block";
   }
    
-  function w3_close() {
+function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
   }
 
-  function myFunction(imgs) {
-    if((document.body.className.match('.sol-lamp-grey'))){
-      var expandImg = document.getElementById("product-img-1");
-      expandImg.src = imgs.src;
-      expandImg.parentElement.style.display = "block";
-    }
-    
-    // }else if((document.body.className.match('.sol-lamp-green'))){
-    //   var expandImg = document.getElementById("product-img-2");
-    //   expandImg.src = imgs.src;
-    //   expandImg.parentElement.style.display = "block";
-
-    // }else if((document.body.className.match('.sol-lamp-brown'))){
-    //   var expandImg = document.getElementById("product-img-3");
-    //   expandImg.src = imgs.src;
-    //   expandImg.parentElement.style.display = "block";
-
-    // }else if((document.body.className.match('.light-wall-v1'))){
-    //   var expandImg = document.getElementById("product-img-4");
-    //   expandImg.src = imgs.src;
-    //   expandImg.parentElement.style.display = "block";
-
-    // }else if((document.body.className.match('.light-wall-v2'))){
-    //   var expandImg = document.getElementById("product-img-5");
-    //   expandImg.src = imgs.src;
-    //   expandImg.parentElement.style.display = "block";
-
-    // }else if((document.body.className.match('.light-path'))){
-    //   var expandImg = document.getElementById("product-img-6");
-    //   expandImg.src = imgs.src;
-    //   expandImg.parentElement.style.display = "block";
-
-    // }else if((document.body.className.match('.light-fen-v1'))){
-    //   var expandImg = document.getElementById("product-img-7");
-    //   expandImg.src = imgs.src;
-    //   expandImg.parentElement.style.display = "block";
-
-    // }else if((document.body.className.match('.light-fen-v2'))){
-    //   var expandImg = document.getElementById("product-img-8");
-    //   expandImg.src = imgs.src;
-    //   expandImg.parentElement.style.display = "block";
-
-    // }else if((document.body.className.match('.light-fen-v3'))){
-    //   var expandImg = document.getElementById("product-img-9");
-    //   expandImg.src = imgs.src;
-    //   expandImg.parentElement.style.display = "block";
-    // }
-  
+function solGrey(imgs) {
+    var expandImg = document.getElementById("product-img-1");
+    expandImg.src = imgs.src;
+    expandImg.parentElement.style.display = "block";
   }
+
+  function solGreen(imgs) {
+    var expandImg = document.getElementById("product-img-2");
+    expandImg.src = imgs.src;
+    expandImg.parentElement.style.display = "block";
+  }
+
+function solBrown(imgs) {
+    var expandImg = document.getElementById("product-img-3");
+    expandImg.src = imgs.src;
+    expandImg.parentElement.style.display = "block";
+  }
+
+
+    
+    
+   
   
